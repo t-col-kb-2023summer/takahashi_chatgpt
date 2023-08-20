5f4b56de02bd8ab820dfb892af7b4cefe24914fb
t-col-kb-2023summer <142647548+t-col-kb-2023summer@users.noreply.github.com>
2023-08-20 19:24:25 +0900

    Update streamlit_app02.py

--- streamlit_app02.py
+++ streamlit_app02.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+if "messages" not in st.session_state:
+    st.session_state["messages"] = ""
 
 def communicate(): # on_changeイベントで呼び出される関数を追加する
       # 1. ここでChatCPT APIを利用し、テキストボックスの内容を投げる
@@ -14,4 +16,3 @@ st.write("ChatGPT APIを使ったチャットボットです。")
 
 st.text_input("メッセージを入力してください。", key="user_input", on_change=communicate)
 st.write(st.session_state["messages"])
-
