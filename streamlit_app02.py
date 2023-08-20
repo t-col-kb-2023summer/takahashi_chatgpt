29bd74a80c5dd8ae105699e2465d43659dd9bba9
t-col-kb-2023summer <142647548+t-col-kb-2023summer@users.noreply.github.com>
2023-08-20 19:32:14 +0900

    Update streamlit_app02.py

--- streamlit_app02.py
+++ streamlit_app02.py
@@ -1,11 +1,14 @@
 import streamlit as st
 
+# session_stateの初期化
 if "messages" not in st.session_state:
     st.session_state["messages"] = ""
 
-def communicate(): # on_changeイベントで呼び出される関数を追加する
-      # 1. ここでChatCPT APIを利用し、テキストボックスの内容を投げる
-      # 2. APIからの応答をセッションに保存する
+
+# on_changeイベントで呼び出される関数
+def communicate():
+      # Todo1. ここでChatCPT APIを利用し、テキストボックスの内容を投げたい
+      # Todo2. APIからの応答をセッションに保存する
       st.session_state["messages"] = st.session_state["user_input"] # 今は仮で入力された文字を保存する
       st.session_state["user_input"] = ""  # 入力欄を消去
 
