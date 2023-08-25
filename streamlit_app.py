d54af761808e71cc4524f91a8ad3bfeed820e1e4
t-col-kb-2023summer <142647548+t-col-kb-2023summer@users.noreply.github.com>
2023-08-25 15:13:47 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,12 +12,11 @@ message_max = st.secrets.ChatSettings.message_max
 # else
 #     role_input = st.session_state[user_role]
 
-if "user_role" not in st.session_state:
-    role_input = st.text_input("ロールを入力してください.", key="user_role")
-if "messages" not in st.session_state and "user_role" in st.session_state:
-    st.session_state["messages"] = [
-        {"role": "system", "content": st.session_state[user_role]}
-      ]
+# if "user_role" not in st.session_state:
+# if "messages" not in st.session_state and "user_role" in st.session_state:
+#     st.session_state["messages"] = [
+#         {"role": "system", "content": st.session_state[user_role]}
+#       ]
 # elif "messages" not in st.session_state and "user_role" not in st.session_state:
 #     st.session_state["role": "system", "content": ""]
 if "messages_len" not in st.session_state:
@@ -27,31 +26,40 @@ if "total_tokens" not in st.session_state:
 if "all_tokens" not in st.session_state:
     st.session_state["all_tokens"] = 0
 
+def set_role():
+    st.session_state["messages"] = [
+            {"role": "system", "content": st.session_state["user_role"]}
+        ]
+    all_messages = st.session_state["messages"]
 
 # チャットボットとやりとりする関数
 def communicate():
-    all_messages = st.session_state["messages"]
+    if "user_role" in st.session_state:
+        all_messages = st.session_state["messages"]
 
-    user_message = {"role": "user", "content": st.session_state["user_input"]}
-    all_messages.append(user_message)
+        user_message = {"role": "user", "content": st.session_state["user_input"]}
+        all_messages.append(user_message)
+        
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=all_messages
+        )
+        
+        assistant_message = response["choices"][0]["message"]
+        all_messages.append(assistant_message)
 
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=all_messages
-    )
-
-    assistant_message = response["choices"][0]["message"]
-    all_messages.append(assistant_message)
-
-    if len(all_messages) >= message_max:
-        del all_messages[1:3] # 最も古いやり取り(質問+応答)を削除(先頭はrole:systemなので削除せず)
-
-    st.session_state["messages_len"] = len(all_messages)
-    st.session_state["total_tokens"] = response["usage"]["total_tokens"]
-    st.session_state["all_tokens"] += response["usage"]["total_tokens"]
-    st.session_state["user_input"] = ""  # 入力欄を消去
+        if len(all_messages) >= message_max:
+            del all_messages[1:3] # 最も古いやり取り(質問+応答)を削除(先頭はrole:systemなので削除せず)
+        
+        st.session_state["messages_len"] = len(all_messages)
+        st.session_state["total_tokens"] = response["usage"]["total_tokens"]
+        st.session_state["all_tokens"] += response["usage"]["total_tokens"]
+        st.session_state["user_input"] = ""  # 入力欄を消去
+    else:
+        st.write("ロールを入力してください")
 
 
+        
 # 現在のやりとりに対するコスト表示
 def display_tokens():
     len = str(st.session_state["messages_len"])
@@ -66,6 +74,7 @@ st.write("ChatGPT APIを使ったチャットボットです。")
 display_tokens()
     
 user_input = st.text_input("メッセージを入力してください。", key="user_input", on_change=communicate)
+role_input = st.text_input("ロールを入力してください.", key="user_role", on_change=set_role)
 
 if st.session_state["messages"]:
     all_messages = st.session_state["messages"]
