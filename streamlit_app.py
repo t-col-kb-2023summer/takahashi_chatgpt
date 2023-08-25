08f1ed4d292e67ce634ff17ecf1f23acf91b290f
t-col-kb-2023summer <142647548+t-col-kb-2023summer@users.noreply.github.com>
2023-08-25 14:53:22 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,8 @@ if "messages" not in st.session_state and "user_role" in st.session_state:
     st.session_state["messages"] = [
         {"role": "system", "content": st.session_state[user_role]}
         ]
-# elif "messages" not in st.session_state and "user_role" not in st.session_state:
-#     st.session_state["role": "system", "content": ""]
+elif "messages" not in st.session_state and "user_role" not in st.session_state:
+    st.session_state["role": "system", "content": ""]
 if "messages_len" not in st.session_state:
     st.session_state["messages_len"] = 0
 if "total_tokens" not in st.session_state:
